3c564c3f803a09881dc41e3a3d4bb1e5bceeeed5
Sarp Aktug <sarp@singleton.ee>
2022-01-28 00:51:47 +0200

    removed mock data

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo_app/category/category.dart';
 import 'package:flutter_todo_app/screens/main_screen.dart';
-import 'package:flutter_todo_app/todo/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,24 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Category> categories = [
-      Category(1, 'Home', Icons.home, Colors.amber, [
-        Todo(11, 'Do shopping', false),
-        Todo(12, 'Do shopping', true),
-        Todo(13, 'Do shopping', false),
-        Todo(14, 'Do shopping', false)
-      ]),
-      Category(2, 'Work', Icons.work, Colors.green, []),
-      Category(3, 'School', Icons.school, Colors.orange, [])
-    ];
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainScreen(
-        categories: categories,
-      ),
+      home: const MainScreen(),
     );
   }
 }
