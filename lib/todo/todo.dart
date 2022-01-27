02b8aff4ddad6821c369098ec74228efc0c3a3ef
Sarp Aktug <sarp@singleton.ee>
2022-01-28 00:35:30 +0200

    added json methods to todo

--- lib/todo/todo.dart
+++ lib/todo/todo.dart
@@ -1,7 +1,29 @@
+import 'dart:convert';
+
 class Todo {
   int id;
   String desc;
   bool done;
 
   Todo(this.id, this.desc, this.done);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'desc': desc,
+      'done': done,
+    };
+  }
+
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      map['id']?.toInt() ?? 0,
+      map['desc'] ?? '',
+      map['done'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Todo.fromJson(String source) => Todo.fromMap(json.decode(source));
 }
